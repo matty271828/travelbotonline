f1154fe47635b19ddbd0b7c25f22b5d34f45e418
Matthew Maclean <macleanmatty@gmail.com>
2021-04-06 20:58:31 +0100

    Provisioned heroku database addon

--- application.py
+++ application.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import os
+import psycopg2
 
 # Configure application
 app = Flask(__name__)
+
+# Connect to database
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 @app.route("/")
 def index():
