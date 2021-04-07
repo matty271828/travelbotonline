ef4b11895b46e908b71c9665560a9d6b851bd1f6
Matthew Maclean <macleanmatty@gmail.com>
2021-04-07 13:55:32 +0100

    Added flask configurations

--- application.py
+++ application.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 import os
 import psycopg2
 
 # Configure application
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+response.headers["Expires"] = 0
+response.headers["Pragma"] = "no-cache"
+return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Connect to database
 DATABASE_URL = os.environ['DATABASE_URL']
-
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+
+
 
 @app.route("/")
 def index():
@@ -16,5 +38,3 @@ def index():
 
 if __name__ == "__main__":
     app.run()
-
-# Test comment 1
