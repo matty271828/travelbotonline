d7f100bb0301d041c9f97d1f0a5d6cfea236730c
Matthew Maclean <macleanmatty@gmail.com>
2021-04-08 20:57:01 +0100

    flask updates

--- application.py
+++ application.py
@@ -2,15 +2,32 @@ import os
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from tempfile import mkdtemp
 import psycopg2
 
 # Configure application
 app = Flask(__name__)
 
+ENV = 'dev'
+# Run local database
+if ENV == 'dev':
+	app.debug == True
+	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('local_database_url')
+
+# Run production database
+else:
+	app.debug == False
+	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('production_database_url')
+
+# Set track modifications to false
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Create database object
+db = SQLAlchemy(app)
 
 # Ensure responses aren't cached
 @app.after_request
