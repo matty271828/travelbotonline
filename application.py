1f13e8c1b0aa1fe6adfdd7ab13cf27923cde889e
Matthew Maclean <macleanmatty@gmail.com>
2021-04-10 17:03:34 +0100

    Added run_sql.py

--- application.py
+++ application.py
@@ -2,42 +2,16 @@ import os
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
 from tempfile import mkdtemp
 import psycopg2
+import psycopg2.extras as ext
+from run_sql import run_sql
 
 # Configure application
 app = Flask(__name__)
 
-# Set environment variable
-ENV = 'dev'
-
-# Run local database
-if ENV == 'dev':
-	app.debug == True
-	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('local_database_url')
-
-# Run production database
-else:
-	app.debug == False
-	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('production_database_url')
-
-# Set track modifications to false
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# Create database object
-db = SQLAlchemy(app)
-
-class Feedback(db.Model):
-    __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Integer)
-
-    def __init__(self, amount):
-    	self.amount = amount
 
 # Ensure responses aren't cached
 @app.after_request
@@ -56,14 +30,18 @@ Session(app)
 @app.route("/")
 def index():
 	"""Renders index page"""
-	return render_template("index.html", test_value)
+	return render_template("index.html")
 
 @app.route("/login")
 def login():
 	"""Log user in"""
-	test_value = db.column(1)
+	sql = "SELECT * FROM test_table"
 
-	return render_template("login.html", test_value)
+	results = run_sql(sql)
+
+	print(results)
+
+	return render_template("login.html")
 
 if __name__ == "__main__":
     app.run()
---
+++ run_sql.py
@@ -0,0 +1,32 @@
+import psycopg2
+import psycopg2.extras as ext
+import os
+
+# Set environment variable
+ENV = 'dev'
+# Run local database
+if ENV == 'dev':
+    DATABASE_URL = os.environ.get('local_database_url')
+# Run production database
+else:
+    DATABASE_URL = os.environ.get('production_database_url')
+
+# Function to run SQL queries 
+def run_sql(sql, values = None):
+    conn = None
+    results = []
+
+    try:
+        # conn=psycopg2.connect("dbname='lingua_snaps'")
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor(cursor_factory=ext.DictCursor)
+        cur.execute(sql, values)
+        conn.commit()
+        results = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return results
