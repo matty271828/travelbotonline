cea1a9197a8614e7295b2ea3331b8cb85de6fe75
Matthew Maclean <macleanmatty@gmail.com>
2021-04-10 15:12:56 +0100

    SQLAlchemy DB Approach

--- application.py
+++ application.py
@@ -9,7 +9,9 @@ import psycopg2
 # Configure application
 app = Flask(__name__)
 
+# Set environment variable
 ENV = 'dev'
+
 # Run local database
 if ENV == 'dev':
 	app.debug == True
@@ -29,6 +31,14 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Create database object
 db = SQLAlchemy(app)
 
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer)
+
+    def __init__(self, amount):
+    	self.amount = amount
+
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
@@ -43,19 +53,17 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Connect to database
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
 @app.route("/")
 def index():
 	"""Renders index page"""
-	return render_template("index.html")
+	return render_template("index.html", test_value)
 
 @app.route("/login")
 def login():
 	"""Log user in"""
-	return render_template("login.html")
+	test_value = db.column(1)
+
+	return render_template("login.html", test_value)
 
 if __name__ == "__main__":
     app.run()
