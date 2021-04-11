9a51df632ba5911e4565c5afd1ee4cf3a263b412
Matthew Maclean <macleanmatty@gmail.com>
2021-04-11 20:37:15 +0100

    Added apology page functionality

--- application.py
+++ application.py
@@ -7,6 +7,8 @@ import psycopg2
 import psycopg2.extras as ext
 from run_sql import run_sql
 
+from helpers import apology
+
 # Configure application
 app = Flask(__name__)
 
@@ -32,21 +34,44 @@ def index():
 	"""Renders index page"""
 	return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
 	"""Log user in"""
-
-	# SQL example code commented out for later use
-	# sql = "SELECT * FROM test_table"
-	# results = run_sql(sql)
-	# print(results)
-
 	return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
 	"""Register user account"""
-	return render_template("register.html")
+	# User reached via post
+	if request.method == 'POST':
+
+		# Ensure username was submitted
+		if not request.form.get("username"):
+			return apology("Must provide username", 400)
+
+		# Ensure password was submitted
+		elif not request.form.get("password"):
+			return apology("Must provide password", 400)
+
+		# Ensure confirm password was submitted
+		elif not request.form.get("confirmation"):
+			return apology("Must confirm password", 400)
+
+		# Ensure confirm password matches password
+		elif request.form.get("password") != request.form.get("confirmation"):
+			return apology("Password does not match password confirmation", 400)
+
+		else:
+			return render_template("register.html")
+
+	# User reached via get
+	else:
+		return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
+
+# SQL example code commented out for later use
+# sql = "SELECT * FROM test_table"
+# results = run_sql(sql)
+# print(results)
