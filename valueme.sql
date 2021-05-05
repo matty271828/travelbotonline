d32ba1357652a2d01521b5e026706e4783b4c4f5
Matthew Maclean <macleanmatty@gmail.com>
2021-05-05 21:55:40 +0100

    Configured for production

--- valueme.sql
+++ valueme.sql
@@ -2,4 +2,4 @@ DROP TABLE users
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, hash TEXT);
 
-CREATE TABLE watchlist_requests (request_id SERIAL PRIMARY KEY UNIQUE NOT NULL, ticker TEXT, user_id INTEGER);
+CREATE TABLE watchlist_requests (request_id SERIAL PRIMARY KEY, ticker TEXT, user_id INTEGER);
