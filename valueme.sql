8a545275ae7265defc3f650b6b2a09d18932ae3e
Matthew Maclean <macleanmatty@gmail.com>
2021-08-07 20:11:42 +0100

    Removed valueme db file

--- valueme.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE users
-
-CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, hash TEXT);
-
-CREATE TABLE watchlist_requests (request_id SERIAL PRIMARY KEY, ticker TEXT, user_id INTEGER);
